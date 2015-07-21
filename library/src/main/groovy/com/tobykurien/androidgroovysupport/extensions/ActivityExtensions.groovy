6a0c81151896f3d640f62d29f55e36daebe4f9de
Toby Kurien <toby@tobykurien.com>
2015-07-21 15:46:35 +0200

    added ActivityExtensions that adds "toast" and "confirm" methods to Activities and Fragments

---
+++ library/src/main/groovy/com/tobykurien/androidgroovysupport/extensions/ActivityExtensions.groovy
@@ -0,0 +1,55 @@
+package com.tobykurien.androidgroovysupport.extensions
+
+import android.app.Activity
+import android.app.AlertDialog
+import android.content.Context
+import android.support.v4.app.Fragment
+import android.widget.Toast
+
+class ActivityExtensions {
+    static void toastShort(Context self, String message) {
+        toastMessage(self, message, Toast.LENGTH_SHORT)
+    }
+
+    static void toast(Context self, String message) {
+        toastMessage(self, message, Toast.LENGTH_LONG)
+    }
+
+    static toastMessage(Context self, String message, int length) {
+        Toast.makeText(self, message, length).show()
+    }
+
+    static void toastShort(Fragment self, String message) {
+        toastMessage(self.activity, message, Toast.LENGTH_SHORT)
+    }
+
+    static void toast(Fragment self, String message) {
+        toastMessage(self.activity, message, Toast.LENGTH_LONG)
+    }
+
+    static void toastShort(android.app.Fragment self, String message) {
+        toastMessage(self.activity, message, Toast.LENGTH_SHORT)
+    }
+
+    static void toast(android.app.Fragment self, String message) {
+        toastMessage(self.activity, message, Toast.LENGTH_LONG)
+    }
+
+    static void confirm(Fragment self, String message, Closure onConfirm) {
+        confirm(self.activity, message, onConfirm)
+    }
+
+    static void confirm(android.app.Fragment self, String message, Closure onConfirm) {
+        confirm(self.activity, message, onConfirm)
+    }
+
+    static void confirm(Activity self, String message, Closure onConfirm) {
+        new AlertDialog.Builder(self)
+                .setMessage(message)
+                .setPositiveButton(android.R.string.ok, {di, i -> onConfirm.call()})
+                .setNegativeButton(android.R.string.cancel, null)
+                .create()
+                .show()
+    }
+
+}
